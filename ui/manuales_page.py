f68ffdad472ab4e0133b1d4ee08552089caee489
yogomon <64658117+yogomon@users.noreply.github.com>
2025-08-10 20:02:59 +0200

    Update manuales_page.py

--- ui/manuales_page.py
+++ ui/manuales_page.py
@@ -123,7 +123,7 @@ def display_manuales_page():
     tanto los manuales como los informes de análisis estratégico.
     """
     # --- SECCIÓN 1: MANUALES ---
-    st.header("Consulta los Manuales.")
+    st.header("Consulta los Manuales")
     
     base_path = "data"
     manuales_paths = {
@@ -165,7 +165,7 @@ def display_manuales_page():
     #st.divider()
 
     # --- SECCIÓN 2: ANÁLISIS ESTRATÉGICO ---
-    st.header("Explora lo más preguntado.")
+    st.header("Explora lo más preguntado")
     #st.markdown("Selecciona una especialidad para visualizar las 'zonas calientes' del temario.") 
 
     report_prefixes = {
